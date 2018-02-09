614864870d56437955a942e23c9f2bac948384e6
KOITT <KOITT@KOITT-PC13>
2018-02-09 17:22:40 +0900

    Spring 예제 02

---
+++ Spring02ApplicationContext02/src/bean/TestDrive.java
@@ -0,0 +1,30 @@
+package bean;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class TestDrive {
+	
+	public static void main(String[] args) {
+		// MyBean과 TestDrive 간의 의존성이 생겼다
+		MyBean bean = new MyBean();
+		bean.sayHello();
+		
+		// 의존성이 없게 하기 위해 Spring의 DI를 사용해보자
+		
+		// 1. 스프링 설정파일을 불러온다
+		ApplicationContext context = new FileSystemXmlApplicationContext("./src/config/applicationContext.xml");
+											// 프로젝트 경로 기준
+		
+		// 2. 애플리케이션 컨텍스트를 통해서 mybean이라는 이름으로 MuBean 객체를 요청 후 
+		//		생성한 객체를 bean02 변수에 저장
+		MyBean bean02 = (MyBean) context.getBean("mybean");
+		
+		// 3. bean02 객체를 이용하여 메소드 호출
+		bean02.sayHello();
+		
+		
+	}
+
+}
