21e01d8c9e33a360b97384e3084cfe2c083b84f6
KOITT <KOITT@KOITT-PC13>
2018-02-27 17:22:17 +0900

    Spring 예제 18번 - JUnit 적용 전

---
+++ Spring18BoardJUnit/src/com/koitt/board/dao/UsersDao.java
@@ -0,0 +1,30 @@
+package com.koitt.board.dao;
+
+import java.util.List;
+
+import com.koitt.board.model.Users;
+import com.koitt.board.model.UsersException;
+
+public interface UsersDao {
+	
+	public List<Users> selectAll() throws UsersException;
+	
+	public Users select(Integer no) throws UsersException;
+	
+	// 이메일로 사용자의 모든 정보 가져오기
+	public Users selectByEmail(String email) throws UsersException;
+	
+	public void insert(Users user) throws UsersException;
+	
+	public void delete(Integer no) throws UsersException;
+	
+	public void deleteUserType(Integer no) throws UsersException;
+	
+	public void update(Users users) throws UsersException;
+	
+	// users_authority 테이블에 정보를 입력하기
+	public void insertAuthority(Users users) throws UsersException;
+	
+	public Integer selectLastInsertId() throws UsersException;
+	
+}
---
+++ Spring18BoardJUnit/webapp/WEB-INF/views/join-confirm.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html; charset=UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+<head><title>가입을 환영합니다!!!!!!!!!!!!</title></head>
+<body>
+	<h1>${ name }님~ 가 입 을 환 영 합 니 다 ! ! ! ! ! ! ! !  ! ! !  ! ! ! !  ! </h1>
+	<a href="<c:url value='/index.do'/>">홈으로 이동</a>
+</body>
+</html>
