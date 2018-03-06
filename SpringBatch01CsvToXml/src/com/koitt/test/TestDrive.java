1c162d936f240ccc0c16b6830ce5fd9ff92e42e3
KOITT <KOITT@KOITT-PC13>
2018-03-06 13:39:19 +0900

    Spring Batch 실습예제 01번 - JobParameters를 사용하지 않고 파일 경로를 설정할 때

--- SpringBatch01CsvToXml/src/com/koitt/test/TestDrive.java
+++ SpringBatch01CsvToXml/src/com/koitt/test/TestDrive.java
@@ -29,15 +29,16 @@ public class TestDrive {
 			 *  이 JobExecution 에는 Job수행 결과에 대한 내용이 담겨있다
 			 *  (전달할 Parameter가 없을 때)
 			 */
+			JobExecution execution = launcher.run(job, new JobParameters());
 			
 			
 			// 전달할 Parameter가 있을 때
-			String in = "C:/sample/ex01/report.csv";
+			/*String in = "C:/sample/ex01/report.csv";
 			String out = "C:/sample/ex01/report.xml";
 			
 			JobParameters jobParameters = new JobParametersBuilder().addString("inputFile", in).addString("outputFile", out).addString("date", new Date().toString()).toJobParameters();
 			
-			JobExecution execution = launcher.run(job, new JobParameters());
+			JobExecution execution = launcher.run(job, new JobParameters());*/
 			
 			// 종료 상태 출력
 			System.out.println("종료 상태: " + execution.getStatus());
